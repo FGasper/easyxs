d768103a1d0d62cbcfbcd435198a35b80abf5b68
Felipe Gasper <felipe@felipegasper.com>
2022-04-10 11:56:41 -0400

    Fix point where list-return SVs pointer was savefree’d.

--- easyxs_perlcall.h
+++ easyxs_perlcall.h
@@ -78,8 +78,6 @@ static inline SV** _easyxs_fetch_list_return (pTHX_ int count) {
     Newx(ret, 1 + count, SV*);
     ret[count] = NULL;
 
-    SAVEFREEPV(ret);
-
     while (count-- > 0) {
         ret[count] = SvREFCNT_inc(POPs);
     }
@@ -87,6 +85,8 @@ static inline SV** _easyxs_fetch_list_return (pTHX_ int count) {
     PUTBACK;
     FREETMPS;
     LEAVE;
+
+    SAVEFREEPV(ret);
 
     return ret;
 }
